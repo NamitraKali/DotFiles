aee3c57f814083a4ddddd425a3ce8aacf7685277
NamitraKali <knamitra@gmail.com>
2021-01-27 13:56:20 -0500

    First commit from testing virtual machine

---
+++ .config/qtile/colors.py
@@ -0,0 +1,13 @@
+def init_colors():
+    return [["#201E2F", "#201E2F"], # background
+            ["#AFBAE9", "#AFBAE9"], # foreground
+            ["#312E48", "#312E48"],  # color 0
+            ["#5F75D3", "#5F75D3"],  # color 1
+            ["#614FCE", "#614FCE"],  # color 2
+            ["#0D3475", "#0D3475"],  # color 3
+            ["#555D9E", "#555D9E"],  # color 4
+            ["#D9ADCD", "#D9ADCD"],  # color 5
+            ["#6B2B9C", "#6B2B9C"],  # color 6
+            ["#4F7CCE", "#4F7CCE"],  # color 7
+            ["#2B2940", "#2B2940"],  # color 8
+            ["#8798DE", "#8798DE"]]  # color 9
---
+++ ricemood/qtile.py
@@ -0,0 +1,13 @@
+def init_colors():
+    return [["^r@bg^", "^r@bg^"], # background
+            ["^r@V>l(80)^", "^r@V>l(80)^"], # foreground
+            ["^r@DM>da(0.1)^", "^r@DM>da(0.1)^"],  # color 0
+            ["^r@V>l(60)^", "^r@V>l(60)^"],  # color 1
+            ["^r@V>ro(20)^", "^r@V>ro(20)^"],  # color 2
+            ["^r@DV>^", "^r@DV>^"],  # color 3
+            ["^r@M^", "^r@M^"],  # color 4
+            ["^r@LV>ro(30)^", "^r@LV>ro(30)^"],  # color 5
+            ["^r@V>ro(45)>da(0.3)^", "^r@V>ro(45)>da(0.3)^"],  # color 6
+            ["^r@V>ro(350)^", "^r@V>ro(350)^"],  # color 7
+            ["^r@DM>da(0.2)^", "^r@DM>da(0.2)^"],  # color 8
+            ["^r@V>l(70)^", "^r@V>l(70)^"]]  # color 9
